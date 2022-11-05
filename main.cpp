39adcd7be0b0a9a0ef66d2ed130db424057d0e51
Fernando Lopez <flopez00032@gmail.com>
2022-11-05 15:41:45 -0500

    coumputer in and computer out

--- main.cpp
+++ main.cpp
@@ -23,8 +23,33 @@ int main(int argc, char** argv)
 
     bool married = true;
     short int smallNum = 10;
+    char myGrade = 'A';
 
-    
+    float f1 = 1.111111;
+    float f2 = 1.111111;
+
+    string q1 = "Enter a Number :";
+    string num1, num2;
+    cout << q1;
+    cin>> num1;
+    cout<< "Enter Another Number : ";
+    cin >> num2; 
+    int nNum1 = stoi(num1);
+    int nNum2 = stoi(num2);
+
+    printf("%d + %d = %d\n",nNum1,nNum2,(nNum1+nNum2));
+
+
+    // string formating is almost the same as java
+    printf("Sum = %.7f\n",(f1+f2));
+
+    auto whatAmI = true; 
+
+    cout << "int byte :" << sizeof(int) << endl;
+
+
+
+
     cout << "min unsigned short int " << 
             numeric_limits<short int>::min()
         << endl;
