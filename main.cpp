8123016ad36bda67e212edd57cfeddbdd1968a38
Fernando Lopez <flopez00032@gmail.com>
2022-11-05 15:28:53 -0500

    data types

--- main.cpp
+++ main.cpp
@@ -1,9 +1,46 @@
 #include <iostream>
+#include <cstdlib>
+#include <string>
+#include <limits>
+#include <vector>
+#include <sstream>
+#include <numeric>
+#include <ctime>
+#include <cmath>
 
+// here we use the standard namespace 
 using namespace std;
 
-int main()
+// variable outside function
+int imGlobal = 0; 
+const double PI = 3.141;
+
+int main(int argc, char** argv)
 {
     cout<< "hello my c++ world";
+
+    // data type 
+
+    bool married = true;
+    short int smallNum = 10;
+
+    
+    cout << "min unsigned short int " << 
+            numeric_limits<short int>::min()
+        << endl;
+     cout << "max unsigned short int " << 
+            numeric_limits<short int>::max()
+        << endl;
+     cout << "min unsigned double " << 
+            numeric_limits<double>::min()
+        << endl;
+     cout << "max unsigned double " << 
+            numeric_limits<double>::max()
+        << endl;
+    
+    
+
+
+
     return 0;
 }
